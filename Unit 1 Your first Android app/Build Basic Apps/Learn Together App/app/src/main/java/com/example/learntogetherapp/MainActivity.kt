47732af23d527a6b4670a35781c269adf6bfbed6
naumanbutt2002 <nauman100percent@gmail.com>
2024-07-20 06:17:20 -0400

    Complete Learn Together App

--- Unit 1 Your first Android app/Build Basic Apps/Learn Together App/app/src/main/java/com/example/learntogetherapp/MainActivity.kt
+++ Unit 1 Your first Android app/Build Basic Apps/Learn Together App/app/src/main/java/com/example/learntogetherapp/MainActivity.kt
@@ -3,13 +3,24 @@ package com.example.learntogetherapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.learntogetherapp.ui.theme.LearnTogetherAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +33,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    UIFunction(stringResource(R.string.heading_text),
+                        stringResource(R.string.first_para),
+                        stringResource(R.string.second_para)
+                        )
                 }
             }
         }
@@ -30,17 +44,45 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun UIFunction(heading: String, first: String,second: String, modifier: Modifier = Modifier) {
+    Column (){
+        Image(
+            painter = painterResource(id = R.drawable.bg_compose_background),
+            contentDescription = null,
+            modifier=Modifier.fillMaxWidth()
+        )
+
+        Text(
+            text=heading,
+            fontSize = 24.sp,
+            modifier= Modifier.padding(16.dp)
+
+        )
+        Text(
+            text=first,
+            modifier= Modifier.padding(16.dp,0.dp),
+            textAlign = TextAlign.Justify,
+
+            )
+        Text(
+            text=second,
+            modifier= Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify,
+
+            )
+    }
+
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true,
+    showSystemUi=true,
+    name="My Preview")
 @Composable
-fun GreetingPreview() {
+fun UIFunctionPreview() {
     LearnTogetherAppTheme {
-        Greeting("Android")
+        UIFunction(stringResource(R.string.heading_text),
+            stringResource(R.string.first_para),
+            stringResource(R.string.second_para)
+        )
     }
 }
